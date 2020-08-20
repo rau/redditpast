30379fbf593a7dbfdaedaf1a1df7437d08966d77
Raunak <raunakdaga@gmail.com>
2020-08-20 16:26:13 -0400

    Basics

--- redditpast/home/home.py
+++ redditpast/home/home.py
@@ -1,4 +1,7 @@
+import requests
+import praw
 from flask import Blueprint, render_template
+from pprint import pprint
 
 home_bp = Blueprint(
     'home_bp',
@@ -8,5 +11,22 @@ home_bp = Blueprint(
 )
 
 @home_bp.route('/')
+@home_bp.route('/home/')
 def home():
-    return render_template('index.html')
+    x = requests.get(f"https://api.pushshift.io/reddit/search/submission/?size=2")
+    posts = []
+    rank = 1
+    for post in x.json()['data']:
+        posts.append({
+            'title': post['title'],
+            'url': post['url'],
+            'score': post['score'],
+            'num_comments': post['num_comments'],
+            'rank': rank
+        })
+        rank += 1
+    return render_template('index.html', posts=posts)
+
+@home_bp.route('/faq/')
+def faq():
+    return render_template('faq.html')
